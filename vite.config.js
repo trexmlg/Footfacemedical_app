f021c50c7524bf14440b02e9713660f67b2f0ade
trexmlg <kristersvisendorfs1@gmail.com>
2026-09-09 23:21:36 +0300

    Change Vite build output directory to 'dist' for Vercel static deploy

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,8 @@ export default defineConfig({
             ignored: ['**/storage/framework/views/**'],
         },
     },
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true,
+    },
 });
